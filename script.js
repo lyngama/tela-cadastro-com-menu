70bd7b79813c3aeb5fb2864d99b2ea793b7a4950
Lynton Gama de Souza <lyngama@MacBook-Air-de-Lynton.local>
2023-10-04 15:51:39 -0300

    login form

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btn-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    container.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    container.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    container.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    container.classList.remove('active-popup');
+});
